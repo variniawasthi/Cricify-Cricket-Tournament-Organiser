fe14918d09fca4191e0dd58ef2c17ec8e1778500
vanshika-3101 <vanshikasrivastava4941@gmail.com>
2024-09-18 16:23:40 +0530

    updateto: changes in TournamentCards

--- src/components/TournamentCards.jsx
+++ src/components/TournamentCards.jsx
@@ -1,7 +1,69 @@
-const TournamentCards = () => {
-  return (
-    <div>TournamentCards</div>
-  )
-}
+import { useState } from "react";
+import { FiArrowRight } from "react-icons/fi";
 
-export default TournamentCards
+const TournamentCards = () => {
+  const [activeTab, setActiveTab] = useState("Overview");
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "Overview":
+        return <div>Overview Content</div>;
+      case "Matches":
+        return <div>Matches Content</div>;
+      case "Teams":
+        return <div>Teams Content</div>;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="bg-white rounded-xl m-[1rem] p-[2rem] sm-custom:p-[1rem] shadow-md sm-custom:m-[1rem]">
+      <h1 className="text-left text-lg font-semibold mb-6 flex justify-start items-center ">
+        Tournament Coverage{" "}
+        <span className="text-lg m-1">
+          {" "}
+          <FiArrowRight className="inline mr-2" />
+        </span>{" "}
+      </h1>
+
+      <div className="flex space-x-4 mb-6 border-b text-sm">
+        <button
+          className={`pb-2 ${
+            activeTab === "Overview"
+              ? "border-b-2 border-blue-600 text-blue-600"
+              : "text-gray-600"
+          }`}
+          onClick={() => setActiveTab("Overview")}
+        >
+          Overview
+        </button>
+        <button
+          className={`pb-2 ${
+            activeTab === "Matches"
+              ? "border-b-2 border-blue-600 text-blue-600"
+              : "text-gray-600"
+          }`}
+          onClick={() => setActiveTab("Matches")}
+        >
+          Matches
+        </button>
+        <button
+          className={`pb-2 ${
+            activeTab === "Teams"
+              ? "border-b-2 border-blue-600 text-blue-600"
+              : "text-gray-600"
+          }`}
+          onClick={() => setActiveTab("Teams")}
+        >
+          Teams
+        </button>
+      </div>
+
+      {/* Tab Content */}
+      <div>{renderTabContent()}</div>
+    </div>
+  );
+};
+
+export default TournamentCards;
